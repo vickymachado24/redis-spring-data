1ae0a67df39adc1700f52dcbf4938519da4c9026
Vsupersolid <vicky.mac.24.vm@gmail.com>
2025-03-23 14:15:29 -0700

    Intial commit message

---
+++ src/main/java/com/machado/redis_spring_data/RedisSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.machado.redis_spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/machado/redis_spring_data/RedisSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.machado.redis_spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
